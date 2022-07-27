65861fa379a99a72013ce3db6f069e659b8b999d
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-07-27 18:19:29 +0400

    more roadmap stuff

---
+++ roadmap-config.js
@@ -0,0 +1,15 @@
+//simplist
+var sl_block = 450 
+var sl_item = 333
+var sl_entity = 44
+
+//roundsita
+var rd_block = 123 
+var rd_item = 333
+var rd_entity = 44
+
+//stylizta
+var stl_block = 23 
+var stl_item = 333
+var stl_entity = 44
+
