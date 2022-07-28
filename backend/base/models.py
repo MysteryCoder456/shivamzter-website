8cba564571664d912d702bbfebb02cec581f7f1d
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-07-28 11:11:01 +0400

    added RoadmapItem model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,19 @@
+from enum import Enum
 from django.db import models
 
-# Create your models here.
+
+class PageChoices(Enum):
+    Simplista = "Simplista"
+    Roundista = "Roundista"
+    Stylizta = "Stylizta"
+
+
+class RoadmapItem(models.Model):
+    page = models.TextField(
+        choices=[(choice.name, choice.value) for choice in PageChoices]
+    )
+    block_name = models.TextField(null=False)
+    block_id = models.IntegerField(null=False)
+    alt_names = models.TextField(null=True)
+    thumbnail = models.ImageField(null=False)
+    # TODO: Carousel images...
